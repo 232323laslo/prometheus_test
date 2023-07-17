b2c7b05e0fe399d0a283ccde7d8e6e0745eefa0c
laslo23 <vladyslav_hryshchenko@proton.me>
2023-07-17 15:20:29 +0300

    I am practicing

---
+++ tests/api/test_http.py
@@ -0,0 +1,23 @@
+import pytest
+import requests
+
+@pytest.mark.http
+def test_first_requests():
+    r = requests.get('https://api.github.com/zen')
+    print(f'Response is {r.text}')
+
+@pytest.mark.http
+def test_second_requests():
+    r = requests.get('https://api.github.com/users/defunkt')
+    body = r.json()
+    headers = r.headers
+
+    assert body ['name'] == 'Chris Wanstrath'
+    assert r.status_code == 200
+    assert headers['Server'] == 'GitHub.com'
+
+@pytest.mark.http
+def test_status_code_requests():
+    r = requests.get('https://api.github.com/users/sergii butenko')
+
+    assert r.status_code == 404
